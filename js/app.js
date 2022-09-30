37e297dafc80194c05dc14e6cad6721b2939aa23
jhorman9 <jhosmita9@gmail.com>
2022-09-30 18:32:01 -0500

    actualizando

--- js/app.js
+++ js/app.js
@@ -5,7 +5,15 @@ function playSound(e) {
     const elementPressed = document.querySelector(`.${keyPressed}`)
     
     const soundName = elementPressed.lastElementChild.textContent.toLowerCase(); //para navegar por el DOM obteniendo el ultimo elemento del hijo
+    console.log(soundName)
     const soundToPlay = document.querySelector(`[src="./assets/sounds/${soundName}.wav"]`)
-    console.log(`src="./assets/sounds/${soundName}.wav"`);
     soundToPlay.play();
+}
+
+addEventListener("click", playSoundClick);
+
+function playSoundClick(e) {
+    const valueClick = e.target.lastElementChild.textContent.toLowerCase();//obtengo el nombre de la tecla
+    const soundToPlay = document.querySelector(`[src="./assets/sounds/${valueClick}.wav"]`) //lo llamo desde el html
+    soundToPlay.play(); //lo ejecuto
 }
